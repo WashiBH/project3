11b5d269ba49fa5c526cc5be7caee9389211e0a6
Washington Blas Huaman <washington.blas.h@gmail.com>
2023-09-19 06:20:13 -0500

    api gateway

--- api-gateway/src/main/java/com/project/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/project/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.project.customers;
+package com.project.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,10 +7,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class CustomersApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomersApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
